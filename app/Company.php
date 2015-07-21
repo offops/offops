14617c8dfb575823c1e179e302b17de9ecc29a4d
lkchoi <lester.choi@pith-studio.com>
2015-07-21 04:42:23 -0400

    Fix relations in Workspace, Company, and User models

--- app/Company.php
+++ app/Company.php
@@ -6,13 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+	/**
+	 * This company belongs to a workspace
+	 * @return Relation
+	 */
     public function workspace()
     {
-    	return $this->belongsTo('Company');
+    	return $this->belongsTo('App\Workspace');
     }
 
+    /**
+     * This company has many users
+     * @return Relation
+     */
     public function users()
     {
-    	return $this->hasMany('User');
+    	return $this->hasMany('App\User');
     }
 }
--- app/Workspace.php
+++ app/Workspace.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Workspace extends Model
 {
+	/**
+	 * This workspace has many companies
+	 * @return Relation
+	 */
 	public function companies()
 	{
-		return $this->hasMany('Workspace');
+		return $this->hasMany('App\Company');
 	}
 }
